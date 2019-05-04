dabb55c50a49de8164520c5a60bca01d810253bd
SALİH BESKARDES <32905535+sweetnatured@users.noreply.github.com>
2019-05-05 01:09:11 +0300

    Update MnistClassification.py

--- MnistClassification.py
+++ MnistClassification.py
@@ -10,15 +10,15 @@ import matplotlib.pyplot as plt
 fashion_mnist = keras.datasets.fashion_mnist               #GET THE DATASET FROM KERAS DATABES
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()    #APPOİNT THE LABES FROM DATABASE
 
-train_images= train_images / 255      #MAKE DATA NORMALITION
+train_images= train_images / 255      #MAKE DATA NORMALIZATION
 test_images= test_images/255
 
 model = keras.Sequential([                    #CREATE THE NETWORK STRUCTURE (28*28--128--128--10)
 
     keras.layers.Flatten(input_shape=(28,28)),                  #FLATTEN MAKES ROW DATA FROM İNPUT          
-    keras.layers.Dense(128,activation=tf.nn.tanh),              #CREATES THE FİRST LAYER THAT HAS 128 NEURON     
-    keras.layers.Dense(128,activation=tf.nn.tanh),              #CREATES THE SECOND LAYER THAT HAS 128 NEURON
-    keras.layers.Dense(10,activation=tf.nn.softmax)             #CREATES THE LAST LAYER THAT İS SOFTMAX FUNCTİON TO CALCULATE PROBABİLY DİSTRUBİTİON
+    keras.layers.Dense(128,activation=tf.nn.tanh),              #CREATES THE FİRST LAYER THAT HAS 128 NEURON AND TANH ACT. FUNCTİON     
+    keras.layers.Dense(128,activation=tf.nn.tanh),              #CREATES THE SECOND LAYER THAT HAS 128 NEURON AND TANH ACT. FUNCTİON
+    keras.layers.Dense(10,activation=tf.nn.softmax)             #CREATES THE LAST LAYER THAT HAS SOFTMAX FUNCTİON TO CALCULATE PROBABİLY DİSTRUBİTİON
 
 ])
 
